f08d34906877b4dcbab7c9048371c58e74127c48
Roy Zheng <royzheng2003@gmail.com>
2025-07-30 23:37:29 -0400

    structure file

--- extract_from_data_source.py
+++ extract_from_data_source.py
@@ -1,39 +1,60 @@
 import requests
-import pandas as pd
-import os
-from dotenv import load_dotenv
 import json
+import os
+from datetime import datetime
+from dotenv import load_dotenv
+import pandas as pd
 
-load_dotenv()
 
-API_TENNIS_API_KEY = os.environ['API_TENNIS_API_KEY']
+from google.cloud import storage
 
 
-# extract data from endpoint
-# response = requests.get(
-#     url = "https://api.github.com/search/repositories",
-#     params = {"q": "language:python", "sort":"stars", "order":"desc"}
-# )
+def load_env():
 
-# json_response = response.json()
-# repos = json_response['items']
-# for repo in repos[:5]:
-#     print(repo['name'])
-#     print(repo['description'])
-#     print(repo['stargazers_count'])
-    
-response = requests.get(
-    url = "https://api.api-tennis.com/tennis/?",
-    params = {
-        "action":"get_standings",
-        "APIkey":API_TENNIS_API_KEY,
-        "event_type":'ATP'
-    },
-    verify = False
-)
+    load_dotenv()
 
-json_response = response.json()
-print(json_response)
-print(json_response.keys())
+    API_TENNIS = os.environ['API_TENNIS_API_KEY']
 
-# upload json to data lake
+    GOOGLE_GCS = os.environ['GOOGLE_GCS_API_KEY']
+
+    return API_TENNIS, GOOGLE_GCS
+
+def send_requests():
+    # extract data from endpoint
+    # response = requests.get(
+    #     url = "https://api.github.com/search/repositories",
+    #     params = {"q": "language:python", "sort":"stars", "order":"desc"}
+    # )
+
+    # json_response = response.json()
+    # repos = json_response['items']
+    # for repo in repos[:5]:
+    #     print(repo['name'])
+    #     print(repo['description'])
+    #     print(repo['stargazers_count'])
+        
+    response = requests.get(
+        url = "https://api.api-tennis.com/tennis/?",
+        params = {
+            "action":"get_standings",
+            "APIkey":API_TENNIS_API_KEY,
+            "event_type":'ATP'
+        },
+        verify = False
+    )
+
+    json_response = response.json()
+    print(json_response)
+    print(json_response.keys())
+
+    # upload json to data lake
+
+def upload_to_gcs(data):
+
+    pass
+
+def main():
+    load_env()
+
+    send_requests()
+
