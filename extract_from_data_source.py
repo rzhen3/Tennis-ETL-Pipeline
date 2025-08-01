ecb1b9e566947120f845ff179a97116ca8333b8c
roy zheng <roy.zheng@nrc-cnrc.gc.ca>
2025-08-01 16:06:26 -0400

    small update

--- extract_from_data_source.py
+++ extract_from_data_source.py
@@ -6,19 +6,23 @@ from dotenv import load_dotenv
 import pandas as pd
 
 
-from google.cloud import storage
+# from google.cloud import storage
 
 
 def load_env():
 
     load_dotenv()
 
-    API_TENNIS = os.environ['API_TENNIS_API_KEY']
+    API_TENNIS = os.getenv('API_TENNIS_API_KEY')
+    GOOGLE_GCS = os.getenv('GOOGLE_GCS_API_KEY')
+    SPORT_DEVS = os.getenv('SPORT_DEVS_API_KEY')
 
-    GOOGLE_GCS = os.environ['GOOGLE_GCS_API_KEY']
+    return API_TENNIS, GOOGLE_GCS, SPORT_DEVS
 
-    return API_TENNIS, GOOGLE_GCS
 
+'''
+extract data from endpoint
+'''
 def send_requests():
     # extract data from endpoint
     # response = requests.get(
@@ -32,14 +36,24 @@ def send_requests():
     #     print(repo['name'])
     #     print(repo['description'])
     #     print(repo['stargazers_count'])
+    SPORT_DEVS_URL = "https://tennis.sportdevs.com/rankings?type=eq.atp&class=eq.official"
+    SPORT_DEVS = os.getenv('SPORT_DEVS_API_KEY')
+    payload = {
+        'type': 'atp',
+        'class': 'official'
+    }
+    
+    headers = {
+        'Accept':'application/json',
+        'Authorization': "Bearer " +SPORT_DEVS
+    }
+    
+    print('sending requests..')
         
     response = requests.get(
-        url = "https://api.api-tennis.com/tennis/?",
-        params = {
-            "action":"get_standings",
-            "APIkey":API_TENNIS_API_KEY,
-            "event_type":'ATP'
-        },
+        url = SPORT_DEVS_URL,
+        data = payload,
+        headers = headers,
         verify = False
     )
 
@@ -54,7 +68,9 @@ def upload_to_gcs(data):
     pass
 
 def main():
+    print("hello world")
     load_env()
 
     send_requests()
 
+main()
